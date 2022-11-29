d41b2d61485e97e93447200a1d09d2066e10a9de
Bradley Gichangi <bradgichangi@gmail.com>
2022-11-29 17:26:51 +0000

    Repo page routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Dashboard from './pages/Dashboard'
-import { SignIn } from './pages'
+import { SignIn, Repo } from './pages'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +13,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
-      <Route path="/:user" element={<Dashboard />} />
+      <Route path="/:user" exact element={<Dashboard />} />
+      <Route path="/:user/:repo" element={<Repo />} />
     </Routes>
   )
 }
