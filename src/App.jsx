5216213e131f16b9a70005b451338ab5b1bbb663
bradgichangi <bradgichangi@gmail.com>
2022-11-30 12:16:15 +0000

    Front page styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Dashboard from './pages/Dashboard'
 import { SignIn, Repo } from './pages'
-<<<<<<< HEAD
 import Layout from '../Layout'
-=======
-import Navbar from './layouts/Navbar'
-
->>>>>>> 18d3d786240a9ea5309f6cba4eaed0a9aa148d87
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +12,6 @@ function App() {
 
   
   return (
-<<<<<<< HEAD
     <Layout>
       <Routes>
         <Route path="/" element={<SignIn />} />
@@ -25,17 +19,6 @@ function App() {
         <Route path="/:user/:repo" element={<Repo />} />
       </Routes>
     </Layout>
-=======
-    <>
-    
-    <Routes>
-      <Route path="/" element={<SignIn />} />
-      <Route path="/:user" exact element={<Dashboard />} />
-      <Route path="/:user/:repo" element={<Repo />} />
-    </Routes>
-    </>
-    
->>>>>>> 18d3d786240a9ea5309f6cba4eaed0a9aa148d87
   )
 }
 
