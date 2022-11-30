1b29a16688cb7c62fa312672c0b3925f118158d5
bradgichangi <bradgichangi@gmail.com>
2022-11-30 12:15:41 +0000

    Front page styling

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -6,6 +6,7 @@ const SignIn = ({ username, setUsername }) => {
 
     return (
         <div id='background'>
+            <img src='https://wallpaperaccess.com/full/3414990.jpg' alt="github-background" />
             <div className="username-form">
                 <p>Welcome to the</p>
                 <h1>GitHub Repo Tracker</h1>
